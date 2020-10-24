e30be34944bd47c25ae237d189ff195c93b99612
IkbalAlahdel <isash90@gmail.com>
2020-10-25 02:01:52 +0300

    EES-13 as a customer i want to log in and create account

--- app/views/register.php
+++ app/views/register.php
@@ -4,41 +4,51 @@
   <div class="col-6 col-md-6 col-sm-10 col-xs-10 mx-auto ">
 <div class="card  mx-auto p-3" >
   <h4 class="text-center my-3">Login</h4>
-<form action="signup" method="post">
-  <div class="form-group col-8 mx-auto my-5">
-    <!-- <label for="user_name">Email address:</label> -->
-    <input type="text" class="form-control" placeholder="Enter fist name"  id="first_name" name="first_name" required>
-  </div>
-   <div class="form-group col-8 mx-auto my-5">
-    <!-- <label for="user_name">Email address:</label> -->
-    <input type="text" class="form-control" placeholder="Enter last name"  id="last_name" name="last_name" required>
-  </div>
+<form action="signup" method="post" id="form1" onchange="validation();">
+<div class="col-12 mx-auto my-5 text-center">
+  <div class="inlining-form mx-auto col-4  "> 
+<input type="text" class="txt-form  "name="first_name" id="first_name" placeholder="First name" required/>
+<small id="helpId1" class="text-muted"></small>
+</div>
+<div class="inlining-form mx-auto col-4 "> 
+<input type="text" class="txt-form mx-auto " name="last_name" id="last_name" placeholder="Last name" required/>
+<small id="helpId2" class="text-muted"></small>
+</div>
+</div>
   <div class="form-group col-8 mx-auto my-5">
     <!-- <label for="user_name">Email address:</label> -->
     <input type="text" class="form-control" placeholder="Enter Username"  id="user_name" name="user_name" required>
+    <small id="helpId3" class="text-muted"></small>
   </div>
   <div class="form-group col-8 mx-auto my-5">
     <!-- <label for="user_name">Email address:</label> -->
     <input type="email" class="form-control" placeholder="Enter email"  id="user_email" name="user_email" required>
-    <input type="hidden" class="form-control" placeholder="Enter role"  id="user_role" name="user_role" value="3">
+    <small id="helpId4" class="text-muted"></small>
+    <!--<input type="hidden" class="form-control" placeholder="Enter role"  id="user_role" name="user_role" value="3"> -->
+
   </div>
   <div class="form-group  col-8 mx-auto my-5">
     <!-- <label for="password">Password:</label> -->
     <input type="password" class="form-control" placeholder="Enter Password" id="user_password" name="user_password" required>
+    <small id="helpId5" class="text-muted"></small>
   </div>
   <div class="form-group  col-8 mx-auto my-5">
     <!-- <label for="password">Password:</label> -->
-    <input type="password" class="form-control" placeholder="Enter Password" id="user_password" name="confirm_password" required>
+    <input type="password" class="form-control" placeholder="Confirm Password" id="user_password" name="confirm_password" required>
+    <small id="helpId6" class="text-muted"></small>
   </div>
-  <div class="inline-form form-check  col-8 mx-auto my-3">
-    <label class="form-check-label px-3">
-      <input class="form-check-input" type="checkbox">Remember me
-    </label>
-  <span class="float-right">Forgot <a href="http://localhost/mvcphp/user/forgotpassword">password?</a></span>
+ 
+  
+  <div class="inlining-form  col-12 mx-auto  my-3 align-center text_center">
+  <div class=" inlining col-2 "></div>
+  <div class=" inlining col-4 mx-auto ">
+  <button type="submit" class="bt mx-auto disabled" id="signup_btn">signup</button>
 </div>
-  <div class="inline-form  col-8 mx-auto align-center my-5">
-  <button type="submit" class="btn btn-primary mx-auto rounded ">Submit</button>
-  <a href="http://localhost/mvcphp/user/forgotpassword" class="btn btn-outline mx-auto rounded">password?</a>
+
+  <div class="inlining col-4 mx-auto">
+  <button type="button" class="bt1 mx-auto"><a href="http://localhost/mvcphp/user/login" >login</button></a>
+</div>
+
 </div>
 </form>
 </div>
@@ -66,3 +76,140 @@
     <button type="button" class="cancelbtn">Cancel</button>
     <span class="psw"><a href="http://localhost/mvcphp/user/login">login</a></span>
   </div> -->
+
+
+
+  <script>
+var form1=document.getElementById("form1");
+var sub=document.getElementById("sub");
+
+
+var mass1=document.getElementById("helpId1");
+var mass2=document.getElementById("helpId2");
+var mass3=document.getElementById("helpId3");
+var mass4=document.getElementById("helpId4");
+var mass5=document.getElementById("helpId5");
+var mass6=document.getElementById("helpId6");
+
+var nameFormat= /^[A-Za-z ]+$/;
+var paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,15}$/;
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+var signup_btn = document.querySelector("#signup_btn");
+signup_btn.disabled = true;
+
+
+
+
+function validation(){
+if( nameFormat.test(form1[0].value) && nameFormat.test(form1[1].value) && nameFormat.test(form1[2].value) &&  mailformat.test(form1[3].value) && paswd.test(form1[4].value) && (form1[5].value=== form1[4].value))
+{
+	signup_btn.disabled = false;
+	signup_btn.className = "btn enabled"
+}
+else{
+	signup_btn.disabled = true;
+}
+}
+
+
+form1[0].addEventListener("input",function confirmName() 
+{ 
+    
+if(form1[0].value.match(nameFormat))
+{  
+
+    form1[0].style.borderColor="green";
+return true;
+}
+else
+{
+  mass1.innerHTML = "*Enter field Name ";
+  form1[0].style.borderColor="red";
+return false;
+}
+});
+form1[1].addEventListener("input",function confirmName() 
+{ 
+    
+if(form1[1].value.match(nameFormat))
+{  
+    form1[1].style.borderColor="green";
+return true;
+}
+else
+{
+  mass2.innerHTML = "*Enter field Name ";
+   form1[1].style.borderColor="red";
+return false;
+}
+});
+
+form1[2].addEventListener("input",function confirmName() 
+{ 
+    
+if(form1[2].value.match(nameFormat))
+{
+    form1[2].style.borderColor="green";
+return true;
+}
+else
+{
+  mass3.innerHTML = "*Enter field Name ";
+    form1[2].style.borderColor="red";
+return false;
+}
+});
+
+
+form1[3].addEventListener("input",function confirmEmail() 
+{ 
+if(form1[3].value.match(mailformat))
+{
+    form1[3].style.borderColor="green";
+return true;
+}
+else
+{
+    mass4.innerHTML = "*Enter Field Email ";
+    form1[3].style.borderColor="red";
+return false;
+}
+});
+
+form1[4].addEventListener("input",function CheckPassword() 
+{ 
+if(form1[4].value.match(paswd)) 
+{ 
+    form1[4].style.borderColor="green";
+
+return true;
+}
+else
+{ 
+    mass5.innerHTML = "*  Password Should Contain a Number/Carcter/Special Carcter  ";
+    form1[4].style.borderColor="red";
+
+return false;
+}
+});
+
+form1[5].addEventListener("input",function confirmPass() 
+{ 
+if(form1[5].value!=form1[4].value) 
+{ 
+    mass6.innerHTML = "* Not Match  The password ";
+    form1[5].style.borderColor="red";
+    return false;
+}
+else
+{ 
+    form1[5].style.borderColor="green";
+    return true;
+}
+});
+
+
+
+
+</script>
