5d1664a35a9b49c7b9bdfbc90d80dd4963e57de4
Noof Alwadi <alahoed0@gmail.com>
2020-10-25 14:10:34 +0300

    EES-33 wish list page worked

---
+++ app/controllers/wishlistController.php
@@ -0,0 +1,31 @@
+<?PHP
+use coreAppNS\Controller;
+//include("app/models/category.php");
+class wishlistController extends Controller{
+public $controller;
+public $model;
+
+    function __construct($fun ='wishlist'){
+
+        $this->controller =new Controller();
+        
+       
+
+        $this->controller->view_object->create_view('wishlist');
+      
+       $this->$fun();
+
+       }
+
+      
+    function wishlisT(){
+       
+        
+        $this->controller->view_object->create_view('wishlist');
+      
+    
+    }
+    
+       }
+       ?>
+  
