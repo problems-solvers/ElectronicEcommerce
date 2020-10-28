c415e31592b3dcb2e728f940d9f574f5b798386d
Noof Alwadi <alahoed0@gmail.com>
2020-10-28 20:03:58 +0300

    EES-64 I did the page connect

---
+++ app/controllers/connectController.php
@@ -0,0 +1,31 @@
+<?PHP
+use coreAppNS\Controller;
+//include("app/models/category.php");
+class connectController extends Controller{
+public $controller;
+public $model;
+
+    function __construct($fun ='connect'){
+
+        $this->controller =new Controller();
+        
+       
+
+        $this->controller->view_object->create_view('connect');
+      
+       $this->$fun();
+
+       }
+
+      
+    function  connect(){
+       
+        
+        $this->controller->view_object->create_view('connect');
+      
+    
+    }
+    
+       }
+       ?>
+  
--- app/views/card.php
+++ app/views/card.php
@@ -1,68 +1,9 @@
-
 <div class="nam mx-auto  my-2 py-2 col-4 text-center text-light" style="background-color:#c5aa59">
   <h3>This is a Card</h3>
 </div>
 <br>
 <div class="col-6 my-8 mx-auto">
-<div class="card  mx-auto" style="width:700px; height:200px;">
-  <div class="d-flex align-content-middle">
-    <div class="col-6 px-0 align-items-center ">
-  <img style="height:100%; width:200px;" src="http://placehold.it/64x64" alt="Card image cap">
-  </div>
-  <div class="card-body col-4 py-1 m-auto mx-auto text-left">
-    <h5 class="card-title ">Name</h5>
-    <span >price</span>
-    <br>
-  
-  </div>
-  <div class="col-2 m-auto productCardbtnsV  align-content-middle align-middle" >
-   
-      
-  
-  <div class=" col-lg-4 m-auto close-btn"> <a href="#"  title="instagram">X</a></div>
-         
-          <br>
-          <br><br> 
-          <div class="m-auto col-lg-7" >  <a href="#"  title="instagram"><i class="fas fa-shopping-cart"></i></a></div>            
-  
-  </div>
-  
-  </div>
-</div>
-</div>
-<br><br>
-
-<div class="col-6 my-8 mx-auto">
-<div class="card  mx-auto" style="width:700px; height:200px;">
-  <div class="d-flex align-content-middle">
-    <div class="col-6 px-0 align-items-center ">
-  <img style="height:100%; width:200px;" src="http://placehold.it/64x64" alt="Card image cap">
-  </div>
-  <div class="card-body col-4 py-1 m-auto mx-auto text-left">
-    <h5 class="card-title ">Name</h5>
-    <span >price</span>
-    <br>
-  
-  </div>
-  <div class="col-2 m-auto productCardbtnsV  align-content-middle align-middle" >
-   
-      
-  
-  <div class=" col-lg-4 m-auto close-btn"> <a href="#"  title="instagram">X</a></div>
-         
-          <br>
-          <br><br> 
-          <div class="m-auto col-lg-7" >  <a href="#"  title="instagram"><i class="fas fa-shopping-cart"></i></a></div>            
-  
-  </div>
-  
-  </div>
-</div>
-</div>
-<br><br>
-
-<div class="col-6 my-8 mx-auto">
-<div class="card  mx-auto" style="width:700px; height:200px;">
+<div class="card  mx-auto" style="width:600px; height:200px; border-radius: 20px;">
   <div class="d-flex align-content-middle">
     <div class="col-6 px-0 align-items-center ">
   <img style="height:100%; width:200px;" src="http://placehold.it/64x64" alt="Card image cap">
@@ -71,33 +12,28 @@
     <h5 class="card-title">Name</h5>
     <span >price</span>
     <br>
-  
+   
   </div>
   <div class="col-2 m-auto productCardbtnsV  align-content-middle align-middle" >
    
-      
-  
-  <div class=" col-lg-4 m-auto close-btn"> <a href="#"  title="instagram">X</a></div>
+  <div class=" col-lg-4 m-auto  my-2 close-btn" stayl=" padding:40px "> <a href="#"  title="instagram">X</a></div>
          
-          <br>
-          <br><br> 
-          <div class="m-auto col-lg-7" >  <a href="#"  title="instagram"><i class="fas fa-shopping-cart"></i></a></div>            
-  
+          
+        <br><br><br>
+          <div class="row my-2">   
+          <div class=" mx-auto">   <a href="#"  title="instagram">-</a></div>
+         
+          <div class="mx-auto"> <p> 1 </p></div> 
+          <div class="mx-auto">   <a href="#"  title="instagram">+</a></div>
+         
+         </div>
+          
   </div>
   
   </div>
+</div>
+</div><div class="nam mx-auto  my-4 py-2 col-2 text-center text-light" >
   
-</div>
-<br>
-
-
-  
-<div class="row productCardbtns mx-0"style="margin-top:40px;">
-          <div class="mx-auto">  <a href="#"  title="Facebook"><i class="fas fa-angle-left"></i></a></div> 
-          <div class="mx-auto"> <p>1  2   3  4</p></div> 
-          <div class="mx-auto">  <a href="#"  title="instagram"><i class="fas fa-angle-right"></i></a></div>    
-
-</div>
-</div>
-
-<br>
+      <button class="but" style="background-color:#c5aa59"  >Loge in</button>
+  </div>
+<br><br><br>
